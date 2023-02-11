af178f1666d525b393fb5db1fca7e08cdaa628f5
kdha0727 <dhakim@yonsei.ac.kr>
2023-02-11 17:59:10 +0900

    config: add json-config i/o function

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,27 @@
-from .defaults import default_config
+from .defaults import DEFAULT_CONFIG
 
 
 def load_defaults_config():
-    return dict(default_config)
+    return dict(DEFAULT_CONFIG)
 
 
 def load_defaults_config_key():
-    return default_config.keys()
+    return DEFAULT_CONFIG.keys()
+
+
+def load_json_config(filename):
+    import json
+    cfg = load_defaults_config()
+    with open(filename) as fp:
+        cfg.update(**json.load(fp))
+    return cfg
+
+
+def dump_json_config(cfg, /, filename, *, reduce=True):
+    import json
+    if reduce:
+        cfg = {k: cfg[k] for k, v in DEFAULT_CONFIG.items() if cfg[k] != v}
+    else:
+        cfg = dict(cfg)
+    with open(filename, "w") as fp:
+        json.dump(cfg, fp)
