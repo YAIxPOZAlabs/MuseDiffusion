bdf1caf70a8c46c2c5881cbbdeaccf91218ae1e7
kdha0727 <dhakim@yonsei.ac.kr>
2023-02-12 22:42:55 +0900

    train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-python -u run_train.py --nproc_per_node=4 --master_port=12233 --use_env \
+python -u run_train.py --nproc_per_node=4 --master_port=12233 \
 --config_file train_cfg.json \
 --notes test-commu
 #python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_env run_train.py \
