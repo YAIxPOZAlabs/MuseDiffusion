e85e6239824068570014ddd9f368a84db1d22d9c
kdha0727 <dhakim@yonsei.ac.kr>
2023-02-13 18:57:36 +0900

    make performance better

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@ SOCKETS=`lscpu | grep Socket | awk '{print $2}'`
 TOTAL_CORES=`expr $CORES \* $SOCKETS`
 TOTAL_GPUS=`nvidia-smi -L | wc -l`
 OMP_NUM_THREADS=`expr $TOTAL_CORES / $TOTAL_GPUS`
-OMP_NUM_THREADS=${OMP_NUM_THREADS} python -u run_train.py --nproc_per_node=4 --master_port=12233 \
+OMP_NUM_THREADS=${OMP_NUM_THREADS} python -u run_train.py --nproc_per_node=${TOTAL_GPUS} --master_port=12233 \
 --config_file train_cfg.json \
 --notes test-commu
 #python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_env run_train.py \
