1c905f52ddeae46a8d07ca176e3f50ebac8c0ce6
kdha0727 <dhakim@yonsei.ac.kr>
2023-02-16 02:10:04 +0900

    debug 4

--- scripts/run_decode.py
+++ scripts/run_decode.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='decoding args.')
     parser.add_argument('--model_dir', type=str, default='', help='path to the folder of diffusion model')
     parser.add_argument('--seed', type=int, default=101, help='random seed')
-    parser.add_argument('--step', type=int, default=2000, help='if less than diffusion training steps, like 1000, use ddim sampling')
+    parser.add_argument('--step', type=int, default=1000, help='if less than diffusion training steps, like 1000, use ddim sampling')
 
     parser.add_argument('--bsz', type=int, default=50, help='batch size')
     parser.add_argument('--split', type=str, default='test', choices=['train', 'valid', 'test'], help='dataset split used to decode')
