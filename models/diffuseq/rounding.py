2295f7f7ab00f5ce8b8d8170d27ee35228aae6de
kdha0727 <dhakim@yonsei.ac.kr>
2023-02-16 02:19:30 +0900

    debug 4

--- models/diffuseq/rounding.py
+++ models/diffuseq/rounding.py
@@ -23,12 +23,8 @@ def get_efficient_knn(model_emb, text_emb):
     # print(emb_norm.shape, arr_norm.shape)
     dist = emb_norm + arr_norm.transpose(0, 1) - 2.0 * torch.mm(model_emb, text_emb_t) # (vocab, d) x (d, bsz*seqlen)
     dist = torch.clamp(dist, 0.0, np.inf)
-    print(dist.shape, torch.max(dist), torch.min(dist))
-    topk_out = torch.topk(-dist, k=1, dim=0)
-    import time
-    time.sleep(10)
-    print("SUCCESS")
-    return topk_out.values, topk_out.indices
+    topk_out = torch.topk(-dist.cpu(), k=1, dim=0)
+    return topk_out.values.to(text_emb.device), topk_out.indices.to(text_emb.device)
 
 def rounding_func(text_emb_lst, model, tokenizer, emb_scale_factor=1.0):
     decoded_out_lst = []
